07147e00e1d179a7e63f916cfd746f1a68adc00d
FlppeG. <87151216+FlppeG@users.noreply.github.com>
2025-02-19 03:07:31 -0500

    Added the compilation line.

--- examples/OVA.cpp
+++ examples/OVA.cpp
@@ -1,3 +1,5 @@
+g++ -std=c++17 -fsanitize=undefined OVA2.cpp -I/home/felipeg/whisper.cpp/include -I/home/felipeg/whisper.cpp/ggml/include -L/home/felipeg/whisper.cpp/build/src -lwhisper ../utilities/call_the_model.cpp -o OVA.out -g
+
 #include <iostream>
 #include <fstream>
 #include <vector>
