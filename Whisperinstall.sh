0a0b887f1ac53eb30312b696066d6491ef979101
FlppeG. <87151216+FlppeG@users.noreply.github.com>
2025-02-18 23:31:03 -0500

    Update Whisperinstall.sh

--- Whisperinstall.sh
+++ Whisperinstall.sh
@@ -5,11 +5,13 @@ mkdir build && cd build
 cmake ..
 make -j$(nproc)
 
-#Compilation line (need to adapt to new devices)
-g++ microkey.cpp -I/home/felipeg/whisper.cpp/include -I/home/felipeg/whisper.cpp/ggml/include -L/home/felipeg/whisper.cpp/build/src -lwhisper -o microkey
+#Link to the model used in the program.
+wget https://huggingface.co/ggerganov/whisper.cpp/blob/main/ggml-base.bin
+
+mv ~/home/felipeg/downloads/ggml-base.bin ~/home/felipeg/whisper.cpp/models/
 
 #Command used to configure the PATH of Whisper.cpp shared library.
 export LD_LIBRARY_PATH=/home/felipeg/whisper.cpp/build/src:$LD_LIBRARY_PATH 
 
-#Link to the model used in the program.
-https://huggingface.co/ggerganov/whisper.cpp/blob/main/ggml-base.bin
+#Compilation line (need to adapt to new devices)
+g++ microkey.cpp -I/home/felipeg/whisper.cpp/include -I/home/felipeg/whisper.cpp/ggml/include -L/home/felipeg/whisper.cpp/build/src -lwhisper -o microkey
