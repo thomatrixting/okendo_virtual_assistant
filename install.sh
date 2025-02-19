cdf730ea27a855b1f74f21468d0c86362d56f325
thomas <thomas.quamtum@gmail.com>
2025-02-19 01:57:33 -0500

    maded some sintax better and resolved some basic bugs also i am trying to leave the example to be ready to create the main file ova

--- install.sh
+++ install.sh
@@ -3,3 +3,17 @@ tar -xvzf ollama-linux.tgz
 export PATH="$(pwd)/bin:$PATH" #make ollama and comands on unitlitis work
 ollama serve & 
 ollama pull deepseek-coder
+#This commands can be used to install and build Whisper.cpp
+
+git clone https://github.com/ggerganov/whisper.cpp.git
+cd whisper.cpp
+mkdir build && cd build
+cmake ..
+make -j$(nproc)
+
+#Link to the model used in the program.
+sudo wget -P /home/felipeg/whisper.cpp/models https://huggingface.co/ggerganov/whisper.cpp/resolve/main/ggml-base.bin
+
+#Command used to configure the PATH of Whisper.cpp shared library.
+export LD_LIBRARY_PATH=/home/felipeg/whisper.cpp/build/src:$LD_LIBRARY_PATH 
+
